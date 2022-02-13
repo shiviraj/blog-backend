bb945d79c58a4e30a7fc47de41e4cc0c3e258200
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-02-13 09:47:31 +0530

    Added role based authorization

--- src/main/kotlin/com/blog/security/authorization/AuthorizationAspect.kt
+++ src/main/kotlin/com/blog/security/authorization/AuthorizationAspect.kt
@@ -1,41 +1,30 @@
 package com.blog.security.authorization
 
-
 import com.blog.domain.User
 import com.blog.exceptions.error_code.BlogError
 import com.blog.exceptions.exceptions.UnauthorizedException
-import com.blog.service.UserService
 import org.aspectj.lang.ProceedingJoinPoint
 import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
-import reactor.core.publisher.Mono
 
 @Aspect
 @Component
-class AuthorizationAspect(
-    @Autowired val userService: UserService
-) {
+class AuthorizationAspect {
 
     @Around("@annotation(authorization)")
-    fun performAction(proceedingJoinPoint: ProceedingJoinPoint, authorization: Authorization): Mono<Any> {
-        return Mono.deferContextual { context ->
-            if (context.isEmpty) getUnauthorizedError()
-            else {
-                val headers = context.get("headers") as HttpHeaders
-                val token = headers[HttpHeaders.AUTHORIZATION]!!.first().substringAfter(" ")
-                userService.extractUser(token)
-            }
-        }.flatMap {
-            if (it.role.isJunior(authorization.allowedRole)) getUnauthorizedError()
-            else proceedingJoinPoint.proceed() as Mono<*>
+    fun performAction(proceedingJoinPoint: ProceedingJoinPoint, authorization: Authorization): Any {
+        val args = proceedingJoinPoint.args.toList()
+        if (args.isEmpty()) throw getUnauthorizedError()
+        val user = args.last() as User
+        if (user.role.isJunior(authorization.allowedRole)) {
+            throw getUnauthorizedError()
         }
+        return proceedingJoinPoint.proceed()
     }
 
-    private fun getUnauthorizedError(): Mono<User> {
-        return Mono.error(UnauthorizedException(BlogError.BLOG606))
+    private fun getUnauthorizedError(): UnauthorizedException {
+        return UnauthorizedException(BlogError.BLOG606)
     }
 }
 
