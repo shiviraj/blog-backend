70312d553c90d0b0e02c6a62a12a840db7668a2d
Shivam Rajput <shivam.rajput@thoughtworks.com>
2022-02-17 08:53:06 +0530

    updated truncated content.

--- src/main/kotlin/com/blog/domain/Content.kt
+++ src/main/kotlin/com/blog/domain/Content.kt
@@ -13,8 +13,8 @@ data class Content(
     }
 
     fun getTruncateContent(): String {
-        val content = blocks.filter { it.type == "paragraph" }
-            .joinToString("<br/>") { it.data["text"] as String }
+        val content = blocks.filter { it.type == "paragraph" }.subList(0, 7)
+            .joinToString("\n") { it.data["text"] as String }
         return content.substring(0, content.length.coerceAtMost(500))
     }
 }
